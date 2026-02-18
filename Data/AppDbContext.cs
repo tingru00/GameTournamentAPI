ea75c7a55402352fae5e3807bc69051f1d2ad533
tingru00 <grundbergtindra@gmail.com>
2026-02-18 11:10:59 +0100

    Added DbContext, SQL connection and initial migration.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GameTournamentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameTournamentAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Tournament> Tournaments { get; set; }
+    }
+}
