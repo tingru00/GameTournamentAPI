ba114526213bd431f7ae17a09c53d1f4a5e68b9c
tingru00 <grundbergtindra@gmail.com>
2026-02-19 11:47:51 +0100

    Add POST endpoint and configure Swagger UI.

--- Services/TournamentService.cs
+++ Services/TournamentService.cs
@@ -2,6 +2,7 @@
 using GameTournamentAPI.DTOs;
 using GameTournamentAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace GameTournamentAPI.Services
 {
@@ -21,6 +22,26 @@ namespace GameTournamentAPI.Services
             { Id = t.Id})
             .ToListAsync();
         }
+        public async Task<TournamentResponseDTO> CreateAsync(TournamentCreateDTO dto)
+        {
+            var tournament = new Tournament
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                MaxPlayers = dto.MaxPlayers,
+                Date = dto.Date
+            };
+
+            _context.Tournaments.Add(tournament);
+            await _context.SaveChangesAsync();
+
+            return new TournamentResponseDTO
+            {
+                Id = tournament.Id
+            };
+        }
+
+
 
     }
 }
